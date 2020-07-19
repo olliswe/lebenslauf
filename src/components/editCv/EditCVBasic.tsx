ab9f22264a31f101814e8c5c9ebdc49543bc2eae
olliswe <oliveriyer@gmail.com>
2020-07-19 16:17:20 +0200

    added form wrapper

--- src/components/editCv/EditCVBasic.tsx
+++ src/components/editCv/EditCVBasic.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Title from "../../elements/Title";
+import { FormWrapper } from "./Shared";
 
 const EditCVBasic = () => {
   return (
     <div>
       <Title level={2}>Basic Information</Title>
+      <FormWrapper>Form goes here...</FormWrapper>
     </div>
   );
 };
